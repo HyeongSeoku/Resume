7e5a7815e516122456333a184da8352184c72240
HyeongSeok <gudtjr3437@naver.com>
2024-01-13 17:43:51 +0900

    Modify querySelectorAll 클래스 네임 기반으로 element 선택하도록 변경

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 const io = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    const $target = entry.target;
+    const { target } = entry;
 
+    //FIXME: this syntax 수정 필요
     if (entry.isIntersecting) {
-      $target.classList.add("screen-in");
+      target.classList.add("screen-in");
+    } else {
+      target.classList.remove("screen-in");
     }
-    // else {
-    //     $target.classList.remove("screen-in");
-    //   }
   });
 });
 
-// FIXME: querySelectorAll props 수정 필요함 -> classname 하나 주고 해당 클래스 네임으로 처리 예정
-// const $items = document.querySelectorAll("div");
-// $items.forEach((item) => {
-//   io.observe(item);
-// });
-
-const $items = document.querySelectorAll(".screen-animation");
-$items.forEach((item) => {
+const items = document.querySelectorAll(
+  ".screen-animation, .section-title, .project-corp-title"
+);
+items.forEach((item) => {
   io.observe(item);
 });
 
