04f146b49e7c0caf12a102a7bd42a03cbe751350
HyeongSeoku <gudtjr3437@naver.com>
2024-01-15 13:33:12 +0900

    Modify : IntersectionObserver 관련 불필요한 코드 삭제

--- src/index.js
+++ src/index.js
@@ -2,24 +2,17 @@ const io = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     const { target } = entry;
 
-    //FIXME: this syntax 수정 필요
     if (entry.isIntersecting) {
       target.classList.add("screen-in");
     } else {
-      target.classList.remove("screen-in");
+      // target.classList.remove("screen-in");
     }
   });
 });
 
-const items = document.querySelectorAll(
+const fadeInAnimationitems = document.querySelectorAll(
   ".screen-animation, .section-title, .project-corp-title"
 );
-items.forEach((item) => {
+fadeInAnimationitems.forEach((item) => {
   io.observe(item);
 });
-
-// 특정 요소만 옵저버를 해제.
-// io.unobserve(targetElement);
-
-// 옵저버 전체를 해제.
-// io.disconnect();
