bfa9d0ce9d248c4fd22a1ff6ac92e670fb577ec5
HyeongSeoku <gudtjr3437@naver.com>
2024-01-17 18:41:05 +0900

    Add : Pdf 저장 기능

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import { printPDF } from "./utils";
+
 const io = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     const { target } = entry;
 
     if (entry.isIntersecting) {
+      target.classList.remove("invisible");
       target.classList.add("screen-in");
     } else {
       // target.classList.remove("screen-in");
@@ -10,9 +13,11 @@ const io = new IntersectionObserver((entries) => {
   });
 });
 
-const fadeInAnimationitems = document.querySelectorAll(
-  ".screen-animation, .section-title, .project-corp-title"
-);
+const fadeInAnimationitems = document.querySelectorAll(".screen-animation");
 fadeInAnimationitems.forEach((item) => {
+  item.classList.add("invisible");
   io.observe(item);
 });
+
+const pdfButton = document.querySelector(".pdf-button");
+pdfButton?.addEventListener("click", printPDF);
