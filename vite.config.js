9a45b0faff2cc7e8b5b0603498059d698ac5201b
HyeongSeoku <gudtjr3437@naver.com>
2024-01-18 19:08:46 +0900

    Modify : vite 빌드 에러 해결을 위한 디렉토리 위치 변경

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,19 @@ import { defineConfig } from "vite";
 import * as path from "path";
 
 export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: "src/index.ts",
+    },
+  },
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
   css: {
     preprocessorOptions: {
       scss: {},
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: "src/index.ts",
     },
   },
 });
