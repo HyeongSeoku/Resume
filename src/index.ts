2e5d402249f3d0c0961424d0a7bd502c1feb10fa
HyeongSeoku <gudtjr3437@naver.com>
2024-01-26 13:55:36 +0900

    Modify : 엘리먼트 생성 타이밍 때문에 pdf-button addEventlistener 안되는 이슈 수정

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { createAside, createAsideObserver, upadeAsideDisplay } from "./aside";
 import "./index.scss";
 import { printPDF } from "./utils";
 
-let asideShow = false;
-
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", function () {
   createAside();
-};
+
+  const pdfButton = document.querySelector(".pdf-button");
+  pdfButton?.addEventListener("click", printPDF);
+});
 
 const io = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -26,31 +27,3 @@ fadeInAnimationitems.forEach((item) => {
   item.classList.add("invisible");
   io.observe(item);
 });
-
-const pdfButton = document.querySelector(".pdf-button");
-pdfButton?.addEventListener("click", printPDF);
-
-const burgermenuToggle = () => {
-  asideShow = !asideShow;
-  console.log("asideShow", asideShow);
-
-  if (asideShow) {
-    burgerMenuButton?.classList.remove("visible");
-    burgerMenuButton?.classList.add("invisible");
-
-    asideContainer?.classList.remove("invisible");
-    asideContainer?.classList.add("visible");
-  } else {
-    asideContainer?.classList.add("invisible");
-    asideContainer?.classList.remove("visible");
-
-    burgerMenuButton?.classList.add("visible");
-    burgerMenuButton?.classList.remove("invisible");
-  }
-};
-
-const burgerMenuButton = document.querySelector(".burger-menu-button");
-burgerMenuButton?.addEventListener("click", burgermenuToggle);
-
-const asideContainer = document.querySelector(".aside-container");
-asideContainer?.addEventListener("click", (e) => console.log(e.target));
