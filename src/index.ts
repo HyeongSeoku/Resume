cd2fc13411b5b7a79852f0134b0144466fa83537
HyeongSeoku <gudtjr3437@naver.com>
2024-01-29 13:28:03 +0900

    Modify : pdf 생성용 element 동적으로 생성되도록 변경

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-import { createAside, createAsideObserver, upadeAsideDisplay } from "./aside";
+import { createAside } from "./aside";
 import "./index.scss";
 import { printPDF } from "./utils";
 
+const createPdfElement = () => {
+  const mainContainer = document.querySelector("#main-container");
+
+  const pdfContainer = document.createElement("div");
+  pdfContainer.classList.add("main-container", "pdf-container");
+  pdfContainer.innerHTML = mainContainer.innerHTML;
+
+  const animationEles = pdfContainer.querySelectorAll(".screen-animation");
+  const inivislbeEles = pdfContainer.querySelectorAll(".invisible");
+
+  animationEles.forEach((element) => {
+    element.classList.remove("screen-animation");
+  });
+
+  inivislbeEles.forEach((element) => {
+    element.classList.remove("invisible");
+  });
+
+  document.body.appendChild(pdfContainer);
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   createAside();
+  createPdfElement();
 
   const pdfButton = document.querySelector(".pdf-button");
   pdfButton?.addEventListener("click", printPDF);
