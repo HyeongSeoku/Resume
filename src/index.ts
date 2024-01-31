5c381b348c3debdbff0a6641f4519c2c68ad9bff
HyeongSeoku <gudtjr3437@naver.com>
2024-01-31 13:31:55 +0900

    Modify : contact 영역 추가 및 클립보드 복사 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
   pdfButton?.addEventListener("click", printPDF);
 });
 
+// fadeInAnimation
 const io = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     const { target } = entry;
@@ -38,8 +39,6 @@ const io = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       target.classList.remove("invisible");
       target.classList.add("screen-in");
-    } else {
-      // target.classList.remove("screen-in");
     }
   });
 });
@@ -48,4 +47,28 @@ const fadeInAnimationitems = document.querySelectorAll(".screen-animation");
 fadeInAnimationitems.forEach((item) => {
   item.classList.add("invisible");
   io.observe(item);
+});
+
+// 클립보드
+const contactTextList = document.querySelectorAll(".contact-list-item-text");
+
+const copyTextToClipBoard = (element: HTMLElement) => {
+  const textToCopy = element.innerText;
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(function () {
+      // TOOD: 클립보드 토스트 메세지 show 메소드
+      console.log("클립보드에 복사 성공", textToCopy);
+    })
+    .catch(function (err) {
+      console.error("클립보드 복사 실패:", err);
+    });
+};
+
+contactTextList.forEach((ele) => {
+  if (ele instanceof HTMLElement) {
+    ele.addEventListener("click", () => {
+      copyTextToClipBoard(ele);
+    });
+  }
 });
